87279b28c5466bcaaf900ea2155c6da83680fe24
Benjamin Gibert <contact@benjamingibert.com>
2023-02-18 13:21:12 +0100

    230218_1321

--- login.js
+++ login.js
@@ -10,22 +10,22 @@ loginForm.appendChild(passwordField);
 const submitButton = document.querySelector(".submitButton");
 loginForm.appendChild(submitButton);
 
-submitButton.addEventListener ("click", function(){ 
+submitButton.addEventListener ("click", async function(event){ 
+    event.preventDefault();
     let email = emailField.value;
     let password = passwordField.value;
-    const loginResponse = fetch('http://localhost:5678/api/users/login', {
+    fetch('http://localhost:5678/api/users/login',{
         method:"POST", 
         body: JSON.stringify({"email":email, "password":password}),
         headers: {'Content-Type':'application/json'}
         })
-    loginResponse
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
             if(data.token) {
             console.log("autorisé");
             } else {(console.log("refusé"))
-            }   
+            } 
         })
         .catch((error) => console.error(error))
     })
