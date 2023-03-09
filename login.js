b0d3f5cc4ce5fb21eeef6bc289df0e08e64be74b
Benjamin Gibert <contact@benjamingibert.com>
2023-03-09 16:53:14 +0100

    230309_1652

--- login.js
+++ login.js
@@ -16,6 +16,10 @@ const passwordField = document.querySelector(".passwordField");
 const buttonForm = document.querySelector(".buttonForm");
 const loginForm = document.querySelector(".loginForm");
 
+const alertMessage = document.createElement("p");
+const passwordForm = document.querySelector(".passwordForm");
+passwordForm.appendChild(alertMessage);
+alertMessage.setAttribute("class", "alertMessage");
 
 /* CREATION D'UN EVENEMENT D'ENVOI DU FORMULAIRE lié à l'élément 'submitButton', 
 et exécution d'une requête POST via l'API pour vérification identification------
@@ -25,6 +29,9 @@ loginForm.addEventListener ("submit", async function(event){
 
     // empêchement du navigateur de recharger la page par défault ---------------------------------------------------------------
     event.preventDefault();
+
+    // réinitialisation du message d'alerte d'identifiants incorrects
+    alertMessage.innerText = "";
 
     // stockage des valeurs email et password entrées en input dans le champs de saisie, dans des variables email et password ---
     let email = emailField.value;
@@ -43,18 +50,13 @@ loginForm.addEventListener ("submit", async function(event){
         .then((data) => {
             if(data.token) {
                 console.log("autorisé");
-                /*si authentification réussie, stockage du token en session storage et redirection vers la page d'accueil en mode logged In (cf script.js)*/
+                // si authentification réussie, stockage du token en session storage et redirection vers la page d'accueil en mode logged In (cf script.js)*/
                 window.sessionStorage.setItem(JSON.stringify(data.userId),JSON.stringify(data.token));
                 window.location.href = "index.html";
             } else {
-                /*si authentification échouée, message d'alerte via une boîte de dialogue*/
-                (console.log("refusé"));
-                //window.alert("utilisateur / mot de passe incorrects");
-                const alertMessage = document.createElement("p");
-                const passwordForm = document.querySelector(".passwordForm");
-                passwordForm.appendChild(alertMessage);
+                // si authentification échouée, message d'alerte via une boîte de dialogue
                 alertMessage.innerText = "utilisateur / mot de passe incorrects";
-                alertMessage.setAttribute("class", "alertMessage");
+                
         }
     })
         .catch((error) => console.error(error))
